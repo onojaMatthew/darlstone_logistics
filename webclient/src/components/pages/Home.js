c8439e3de29eee08902167ba2f29a9af9d3f9cf6
Onoja <onojamatthew59@gmail.com>
2020-04-08 03:42:37 +0100

    added carousel

--- webclient/src/components/pages/Home.js
+++ webclient/src/components/pages/Home.js
@@ -1,28 +1,43 @@
 import React from "react";
-import { Button } from "antd"
+import { Button, Col, Row } from "antd"
+import HomeCarousel from "./Carousel";
 
 const Home = () => {
   return(
     <div>
       <section className="wave-container">
-        {/* <h1>Hello, world!</h1>
-        <p>Check out my awesome waves!</p> */}
+        <Row className="justify-content-center">
+          <Col xs="6" xl="6">
+            <h1>Hello, world!</h1>
+          < p>Check out my awesome waves!</p>
+          </Col>
+        </Row>
+        
         <svg id="curve" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-  <path fill="#0099ff" fill-opacity="1" d="M0,192L80,176C160,160,320,128,480,149.3C640,171,800,245,960,256C1120,267,1280,213,1360,186.7L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
-</svg>
+          <path fill="#0099ff" fill-opacity="1" d="M0,192L80,176C160,160,320,128,480,149.3C640,171,800,245,960,256C1120,267,1280,213,1360,186.7L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+        </svg>
       </section>
-      <section className="sec2"></section>
-      <Button type="primary">Just a Click</Button>
+      <section className="sec2">
+        <Row className="justify-content-center">
+          <Col xs="4" xl="6"
+            style={{
+              marginTop: -200,
+              maxHeight: "200px"
+            }}
+          >
+            <HomeCarousel />
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs="4" xl="2">
+            <Button style={{
+              
+            }}>Request Quote</Button>
+          </Col>
+        </Row>
+      </section>
     </div>
   )
 }
 
 export default Home;
-
-{/* <svg id="curve" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1416.99174.01">
-          <path 
-            className="cls-1"
-            d="M0,280.8S283.66, 59, 608.94, 163.56s437.93, 150,808, 10.34V309.54H0V280.8Z"
-            transform="translate(0 -135.53)"
-          />
-        </svg> */}
--- webclient/src/index.js
+++ webclient/src/index.js
@@ -6,6 +6,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import { rootReducer } from "./store/reducers";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "antd/dist/antd.css";
 import './index.css';
 import App from './App';
