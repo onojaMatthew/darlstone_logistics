9fbd7f0bf182487f4f887524ea7771ef00295d89
Onoja <onojamatthew59@gmail.com>
2020-04-10 16:47:26 +0100

    created quote component

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -3,15 +3,31 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './components/pages/Home';
 import Header from './components/pages/Header';
 import Footer from './components/pages/Footer';
+import Quote from './components/pages/Quote';
 
+const styles = {
+  ntf: {
+    textAlign: "center",
+    marginTop: 300,
+    fontSize: 32,
+    fontWeight: "bold"
+  },
+  container: {
+    minHeight: 400
+  }
+}
 class App extends Component{
   render() {
     return (
       <BrowserRouter>
         <Header />
+        <div style={styles.container}>
         <Switch>
           <Route exact path="/" render={(props) => <Home {...props} />} />
+          <Route path="/request" render={(props) => <Quote {...props} />} />
+          <Route path="/*" render={() => <div style={styles.ntf}>404 Page Not Found!!</div>} />
         </Switch>
+        </div>
         <Footer />
       </BrowserRouter>
     );
---
+++ webclient/src/components/pages/Quote.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+const styles = {
+  container: {
+    // minHeight: 300,
+    color: "#333"
+  }
+}
+const Quote = () => {
+  return (
+    <div style={styles.container}>
+      <p>hello everyone</p>
+    </div>
+  )
+}
+
+export default Quote;
